fd0a5575db7e64f9f0bd66c247297f21a858f326
Tyler <tylerkypark@gmail.com>
2026-02-04 14:50:27 -0800

    a

--- Plant.java
+++ Plant.java
@@ -1,5 +1,7 @@
 public class Plant{
     private final String SPECIES;
+    public static final Boolean USESCHLOROPHYLL = true;
+    private static int numPlants;
     private int age;
     private String name;
     private double height;
@@ -9,6 +11,7 @@ public class Plant{
           this.name = name;
           height = 0;
           this.SPECIES = species;
+          numPlants ++;
     }
 
     public String type(){
@@ -27,5 +30,21 @@ public class Plant{
     }
     public String species(){
         return SPECIES;
+    }
+    public static int numPlants(){
+        return numPlants;
+    }
+    public static void main(String[] args) {
+        boolean v = Plant.USESCHLOROPHYLL;
+        Plant b = new Plant("rose", "c");
+        Plant c = new Plant("rose", "c");
+        Plant d = new Plant("rose", "c");
+        System.out.println(d.numPlants());
+        Plant e = new Plant("rose", "c");
+        Plant f = new Plant("rose", "c");
+        System.out.println(Plant.numPlants());
+
+        
+
     }
 }
